3abba687266fb32c19f7ad9c6887afd0f670bb41
Ethan Fast <ethan@ethan-fasts-macbook-pro.local>
2010-06-22 10:32:19 -0600

    ruby code

--- nodes.rb
+++ nodes.rb
@@ -4,7 +4,6 @@ node_num = ARGV[0].to_i
 joke_num = ARGV[1].to_i
 csv_file = File.new(ARGV[2],"w")
 joke_scew = ARGV[3].to_i
-#edge_w = File.new(ARGV[3],"w")
 
 weight_hash = Hash.new
 nodes = (1..node_num).map{|x| (rand*4)-2}
@@ -22,33 +21,26 @@ if joke_scew == 1
 	jokes = nodes.map{|x| ((-1) ** (rand 2))*( (rand) / 4)}
 end
 
+s = ""
 (0..(node_num-1)).each do |nx|
-	s = ""
 	(0..(node_num-1)).each do |ny|
 		w = (rand) * ((1 - (nodes[nx] - nodes[ny]).abs/4.to_f))  
 		weight_hash[[nx,ny]] = w
 		s = s + "#{w},"
 	end
-#	edge_w.puts "#{s}"
 end
 
 def normdist(joke,personality)
-	out = Statistics2.normaldist(-1 * (joke - personality).abs)
-	#puts "#norm computation #{out}"
-	out
+	Statistics2.normaldist(-1 * (joke - personality).abs)
 end
 
 def act_fun(pnum, jnum, signal, signal_hash, nodes, jokes)
+	alpha_decay = 1.5
 	begin
 		sh = signal_hash[[jnum,pnum]]
-		div_factor = signal.to_f ** 1.0
+		div_factor = signal.to_f ** alpha_decay
 		nd = normdist(jokes[jnum],nodes[pnum])
-#		puts "\t\tdetails:"
-#		puts "\t\tsignal hash #{sh}"
-#		puts "\t\tdiv factor #{div_factor}"
-#		puts "\t\tnormdist #{nd}"
 		out = sh * ((nd * 2) / div_factor) / sh
-#		puts "\t\ttotal #{out}"
 		out
 	rescue
 		0
@@ -59,10 +51,8 @@ end
 	(0..(node_num-1)).each do |n|
 		if (rand) < 0.005
 			signal_hash[[j,n]] = 1
-			puts "t"
 		else
 			signal_hash[[j,n]] = 0
-			puts "f"
 		end
 	end
 end
@@ -76,15 +66,6 @@ go=false
 t=t+1
 outinfo = ""
 activated = false
-#(0..(joke_num-1)).each do |j|
-#k = signal_hash.keys.select{|x| x.first == j}.select{|k| signal_hash[k] != 0}.size
-#if k > 0
-#	puts "not empty? #{k}"
-#	outinfo = outinfo + ",#{k}"
-#else
-#	outinfo = outinfo + ",0"
-#end
-#end
 nodes.each_index do |node|
 	activated = false
 	puts "entering on node #{node}"
@@ -92,23 +73,17 @@ nodes.each_index do |node|
 	jokes.each_index do |jk|
 		sig = sig + signal_hash[[jk,node]]
 	end
-#	puts "signal total of #{sig}"
 	jokes.each_index do |jk|
-#		puts "\tlooking at node #{node} joke #{jk}"	
 		activation = act_fun(node,jk,sig,signal_hash,nodes,jokes)
-#		puts "\tactivation total of #{activation}"
 		if (rand) < activation #activated
 			activated = true
 			nodes.each_index do |second_node|
 				if (rand) < weight_hash[[node,second_node]]
 					signal_hash[[jk,second_node]] = signal_hash[[jk,second_node]] + 1
-#					outinfo = outinfo + ",1"
 					sig = sig + 1
 					go=true
 					puts "\t\tsending signals to node #{second_node}"
-#					puts "\t\tsignal for node is now #{signal_hash[[jk,second_node]]}"
 				else
-#					outinfo = outinfo + ",0"
 				end
 			end
 		end
